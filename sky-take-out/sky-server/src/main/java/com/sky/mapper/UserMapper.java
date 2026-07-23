e29d4ac7038967c959ff1ac8fd215a1ff6283762
Assaka <2682728781@qq.com>
2026-07-23 19:03:38 +0800

    订单支付

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -19,4 +20,12 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long id);
 }
