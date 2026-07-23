b5b2da5bbf4dbffaf9574726b64ceaff432e88de
Assaka <2682728781@qq.com>
2026-07-23 22:52:14 +0800

    定时任务

--- sky-take-out/sky-server/src/main/java/com/sky/SkyServerApplication.java
+++ sky-take-out/sky-server/src/main/java/com/sky/SkyServerApplication.java
@@ -3,12 +3,14 @@ package com.sky;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
+@EnableScheduling
 public class SkyServerApplication {
 
     public static void main(String[] args) {
